91f8e770d90ab121637427b6d55542c8bcbe305a
AndresArtiga333 <andresaartiga057@gmail.com>
2024-08-23 17:10:26 -0600

    feat: agregue el atributo de la clase con los metodos requeridos como el metodo normal sobregargado

--- PruebaVespertina/src/org/oliversales/models/Forma.java
+++ PruebaVespertina/src/org/oliversales/models/Forma.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.oliversales.models;
+// Harlin Palacios
+public abstract class Forma {
+     
+    private String color;
+    
+    public Forma(String color){
+        this.color = color;
+    }
 
-/**
- *
- * @author informatica
- */
-public class Forma {
+    public abstract double calcularArea();
+
+    public void calcularArea(String mensaje){
+        System.out.print(mensaje);
+        System.out.print("El area de la froma es de:" + calcularArea());
+    }
+    
+    public void calcularArea(double numero){
+        System.out.print("El area con el numero multiplicador " + numero + "es: " + (calcularArea() * numero));
+    }
     
 }
